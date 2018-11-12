815d22d2053d279331073c5a28692b3208ac6d0b
cathy192 <cathy192@naver.com>
2018-11-12 11:25:43 +0000

    make check.c

--- check.c
+++ check.c
@@ -5,11 +5,14 @@ int is_valid(char *op, char *args)
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
 
-	/********************************/
-    /*  							*/
-	/*         syntax check         */
-    /*  							*/
-	/********************************/
-	
+	char *str1=strtok(args,",");
+	char *str2=strtok(str1,",");
+
+	if(strcmp(str1[0],"%")==0)&&(strcmp(str2[0],"%")==0)//imm to imm
+	||(strcmp(str1[0],"("==0)&&(strcmp(str2[0],"(")==0)// mem to mem
+	||(strcmp(str2[0],"%")!=0) //des!=imm
+	return 0;
+	else
+
 	return 1;
 }
