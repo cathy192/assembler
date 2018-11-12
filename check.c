aa9b0ea140c934d2d989d382877bb9bc7e17395b
cathy192 <cathy192@naver.com>
2018-11-12 12:15:08 +0000

    make trans.c

--- check.c
+++ check.c
@@ -8,9 +8,8 @@ int is_valid(char *op, char *args)
 	char *str1=strtok(args,",");
 	char *str2=strtok(str1,",");
 
-	if(strcmp(str1[0],"%")==0)&&(strcmp(str2[0],"%")==0)//imm to imm
+	if(strcmp(str1[0],"$")==0)&&(strcmp(str2[0],"$")==0)//imm to imm
 	||(strcmp(str1[0],"("==0)&&(strcmp(str2[0],"(")==0)// mem to mem
-	||(strcmp(str2[0],"%")!=0) //des!=imm
 	return 0;
 	else
 
--- trans.c
+++ trans.c
@@ -9,13 +9,22 @@ int instr_trans(char *op, char *args, char* mcode)
 	}
 
 
-	strcpy(mcode, "AB CD EF");
+	else{
+	char *str1=strtok(args,",");
+        char *str2=strtok(str1,",");
 
-	/********************************/
-    /*  							*/
-	/*    generate machine code     */
-    /*  							*/
-	/********************************/
-	
+	if( strcmp(str1[0],"%")==0 && strcmp(str2[0],"%")==0)//reg to reg
+		strcpy(mcode, "a1");
+	else if( strcmp(str1[0],"(")==0 && strcmp(str2[0],"%")==0)//mem to reg
+		strcpy(mcode,"8b");
+	else if( strcmp(str1[0],"-")==0 && strcmp(str2[0],"%")==0)//mem to reg
+		strcpy(mcode,"8b");
+	else if( strcmp(str1[0],"0")==0 && strcmp(str2,"%eax")==0)//mem to reg(eax)
+		strcpy(mcode,"a1");
+	else if( strcmp(str1,"%eax")==0 && strcmp(str2[0],"0")==0)//reg(eax) to mem
+		strcpy(mcode,"a3");
+	else if( strcmp(str1[0],"$")==0 && strcmp(str2[0],"%")==0)//immediate to reg
+		strcpy(mcode,"b8");
+
 	return 1;	
 }
