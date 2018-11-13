f40cb429c6921f9d5dc02f9e29bf4618e219bc65
cathy192 <cathy192@naver.com>
2018-11-13 11:57:28 +0000

    update

--- check.c
+++ check.c
@@ -5,14 +5,28 @@ int is_valid(char *op, char *args)
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
 
-	char *str1=strtok(args,",");
-	char *str2=strtok(str1,",");
+	char index[40],des[40];
+	int k;
+	for(int i=0;i<strlen(args);i++)
+	{
+		if(args[i]==','){
+			break;
+		}
+		index[i]=args[i];
+		k=i;
+	}
+	for(int j=k+2;j<strlen(args);j++)
+		des[j-k-2]=args[j];
 
-	if((strcmp(str1[0],"$")==0)&&(strcmp(str2[0],"$")==0))
-		return 0;
-	else if((strcmp(str1[0],"(")==0)&&(strcmp(str2[0],"(")==0))
-	return 0;
-	else
 
-	return 1;
+	if(des[0]=='$')	return 0;
+	else if(index[0]=='(' &&des[0]=='(') return 0;
+	else if(index[0]=='(' &&des[0]=='0') return 0;
+	else if(index[0]=='0' &&des[0]=='(') return 0;
+        else if(index[0]=='0' &&des[0]=='0') return 0;
+	else if(index[0]=='-' &&des[0]=='(') return 0;
+        else if(index[0]=='-' &&des[0]=='0') return 0;
+
+
+	else return 1;
 }
--- trans.c
+++ trans.c
@@ -2,6 +2,9 @@
 
 int instr_trans(char *op, char *args, char* mcode)
 {
+	char index[20]={"\0"},des[20]={"\0"};
+
+	int k=0;
 	// check syntax 
 	if(!is_valid(op, args)){
 		printf("Error: %s %s is not valid\n", op, args);
@@ -10,21 +13,62 @@ int instr_trans(char *op, char *args, char* mcode)
 
 
 	else{
-	char *str1=strtok(args,",");
-        char *str2=strtok(str1,",");
+	//char index[20],des[20];
 
-	if( strcmp(str1[0],"%")==0 && strcmp(str2[0],"%")==0)//reg to reg
-		strcpy(mcode, "a1");
-	else if( strcmp(str1[0],"(")==0 && strcmp(str2[0],"%")==0)//mem to reg
-		strcpy(mcode,"8b");
-	else if( strcmp(str1[0],"-")==0 && strcmp(str2[0],"%")==0)//mem to reg
-		strcpy(mcode,"8b");
-	else if( strcmp(str1[0],"0")==0 && strcmp(str2,"%eax")==0)//mem to reg(eax)
-		strcpy(mcode,"a1");
-	else if( strcmp(str1,"%eax")==0 && strcmp(str2[0],"0")==0)//reg(eax) to mem
-		strcpy(mcode,"a3");
-	else if( strcmp(str1[0],"$")==0 && strcmp(str2[0],"%")==0)//immediate to reg
-		strcpy(mcode,"b8");
+	for(int i=0;i<strlen(args);i++){
+		if(args[i]==',')
+			break;
+		index[i]=args[i];
+		k=i;
 	}
+
+	for(int j=k+2;j<strlen(args);j++){
+	
+			des[j-k-2]=args[j];
+	}
+
+	//printf("index:%s des:%s\n",index,des);
+
+	if( index[0]=='0' &&index[1]=='x'){
+		if(des[2]=='a'&&des[3]=='x')
+		strcpy(mcode, "a1");
+	}
+	else if( index[0]=='('|| index[strlen(index)-1]==')'){  
+		strcpy(mcode,"8b");
+	}
+	else if( index[0]=='%'&&index[1]=='e'){
+			if(des[0]=='%'&&des[1]=='e'){
+			strcpy(mcode,"89");}
+	}
+	else if( index[0]=='%'&&index[1]=='e'){
+		if(des[0]=='0'&&des[1]=='x'){
+		strcpy(mcode,"a3");}
+	}
+	else if( index[0]=='$'){
+		
+			if(des[2]=='a'&&des[3]=='x')
+				strcpy(mcode,"b8");
+ 
+ 			else if(des[2]=='b'&&des[3]=='x')
+                                 strcpy(mcode,"bb");
+
+			else if(des[2]=='c'&&des[3]=='x')
+				strcpy(mcode,"b9");
+			else  if(des[2]=='d'&&des[3]=='x')
+				 strcpy(mcode,"ba");
+			else if(des[2]=='s'&&des[3]=='p')
+				 strcpy(mcode,"bc");
+			else if(des[2]=='b'&&des[3]=='p')
+				 strcpy(mcode,"bd");
+			else if(des[2]=='s'&&des[3]=='i')
+                                 strcpy(mcode,"be");
+			else if(des[2]=='d'&&des[3]=='i')
+	                        strcpy(mcode,"bf");
+	}
+		else  strcpy(mcode,"error");
+	
+		
+	}
+	printf("index:%s des:%s\n",index,des);
 	return 1;	
 }
